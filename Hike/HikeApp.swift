f502d11d615b812b8585ff745de408538e776269
Henrique Albuquerque <ehf.albuquerque@gmail.com>
2024-06-27 15:06:47 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Henrique Albuquerque on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
